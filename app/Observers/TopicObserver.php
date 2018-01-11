221f10d82efbbfaf6651d5058fd17f3532fdd902
mushishixian <648132694@qq.com>
2018-01-11 06:24:23 +0000

    Prevent XSS injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -12,11 +12,17 @@ class TopicObserver
     public function creating(Topic $topic)
     {
         //
-        $topic->excerpt = make_excerpt($topic->body);
+//        $topic->excerpt = make_excerpt($topic->body);
     }
 
     public function updating(Topic $topic)
     {
         //
+    }
+
+    public function saving(Topic $topic)
+    {
+        $topic->body = clean($topic->body,'user_topic_body');
+        $topic->excerpt = make_excerpt($topic->body);
     }
 }
