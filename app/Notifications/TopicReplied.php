63dccb20eb84162e093ba6ac9268bab7d6ee6a4c
mushishixian <648132694@qq.com>
2018-01-16 06:30:01 +0000

    Notify user in email with Queue supported

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -28,50 +28,51 @@ class TopicReplied extends Notification
     /**
      * Get the notification's delivery channels.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return array
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     public function toDatabase($notifiable)
     {
         $topic = $this->reply->topic;
         //这里主要是为了设置锚点
-        $link = $topic->link(['#reply'.$this->reply->id]);
+        $link = $topic->link(['#reply' . $this->reply->id]);
 
         return [
-            'reply_id' => $this->reply->id,
+            'reply_id'      => $this->reply->id,
             'reply_content' => $this->reply->content,
-            'user_id' => $this->reply->user->id,
-            'user_name' => $this->reply->user->name,
-            'user_avatar' => $this->reply->user->avatar,
-            'topic_link' => $link,
-            'topic_id' => $topic->id,
-            'topic_title' => $topic->title,
+            'user_id'       => $this->reply->user->id,
+            'user_name'     => $this->reply->user->name,
+            'user_avatar'   => $this->reply->user->avatar,
+            'topic_link'    => $link,
+            'topic_id'      => $topic->id,
+            'topic_title'   => $topic->title,
         ];
     }
 
     /**
      * Get the mail representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
     {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+            ->line('你的话题有新回复! ')
+            ->action('Notification Action', url($url));
     }
 
     /**
      * Get the array representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return array
      */
     public function toArray($notifiable)
