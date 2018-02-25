6ef08af3116146e3f9577eca14678fa92b82c036
mushishixian <648132694@qq.com>
2018-02-25 00:46:56 +0000

    user store and verificationCode Store

---
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\VerificationCodeRequest;
+use App\Service\Api\SmsService;
+use Illuminate\Http\Request;
+
+class VerificationCodesController extends Controller
+{
+    //
+    public function store(VerificationCodeRequest $request,SmsService $smsService)
+    {
+        $phone = $request->phone;
+        $key = 'verificationCode_' . str_random(15);
+        $expireAt = now()->addMinute(10);
+
+        if (!app()->environment('production')) {
+            $code='123456';
+        }else{
+            $code = str_pad(random_int(1, 999999), 6, 0, STR_PAD_LEFT);
+            $smsService->handleSendPhoneCode($phone,$code);
+        }
+
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expireAt);
+
+        return $this->response->array(['key' => $key, 'expired_at' => $expireAt->toDateString()])->setStatusCode(201);
+    }
+}
