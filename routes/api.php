1c84e1f3f74b19d54294eb00ea366b7a969bced0
mushishixian <648132694@qq.com>
2018-02-27 02:53:47 +0000

    api/user

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array',
 ], function ($api) {
     $api->group([
         'middleware' => 'api.throttle',
@@ -32,7 +33,7 @@ $api->version('v1', [
             ->name('api.verificationCodes.store');
         $api->post('users', 'UsersController@store')->name('api.users.store');
         //验证码
-        $api->post('captchas','CaptchasController@store')
+        $api->post('captchas', 'CaptchasController@store')
             ->name('api.captchas.store');
         //第三方登录
         $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
@@ -46,5 +47,19 @@ $api->version('v1', [
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
+    });
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit'      => config('api.rate_limits.access.limit'),
+        'expires'    => config('api.rate_limits.access.expires'),
+    ], function ($api) {
+        // 游客可以访问的接口
+
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function ($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
     });
 });
