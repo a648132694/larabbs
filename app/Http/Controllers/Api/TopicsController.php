c136cb06c1f7ec09ebaa9f228a83ebec3814d06a
mushishixian <648132694@qq.com>
2018-02-27 08:06:36 +0000

    topic delete api

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -28,4 +28,13 @@ class TopicsController extends Controller
 
         return $this->response->item($topic, new TopicTransformer());
     }
+
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('update', $topic);
+
+        $topic->delete();
+
+        return $this->response->noContent();
+    }
 }
