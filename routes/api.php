b4814e2ed1d1de3c16cd163a7734c618ab84344b
mushishixian <648132694@qq.com>
2018-02-28 06:46:15 +0000

    replies index

--- routes/api.php
+++ routes/api.php
@@ -65,6 +65,13 @@ $api->version('v1', [
         //个人话题列表
         $api->get('users/{user}/topics', 'TopicsController@userIndex')
             ->name('api.users.topics.index');
+        //回复列表
+        $api->get('topics/{topic}/replies', 'RepliesController@index')
+            ->name('api.topics.replies.index');
+        //某个用户的回复列表
+        $api->get('users/{user}/replies', 'RepliesController@userIndex')
+            ->name('api.users.replies.index');
+
         // 需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function ($api) {
             // 当前登录用户信息
@@ -91,6 +98,7 @@ $api->version('v1', [
             //删除回复
             $api->delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')
                 ->name('api.topics.replies.destroy');
+
         });
     });
 });
